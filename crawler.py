9cf849f5c51ea8c6465b14dcf5659e08ca82fd8d
Xinghao Zhou <zhou.xinghao.1991@gmail.com>
2016-07-16 23:40:26 -0400

    another test

--- crawler.py
+++ crawler.py
@@ -1,30 +1,8 @@
+import requests
+import re
 
-from selectors import DefaultSelector, EVENT_WRITE
+r = requests.get("http://www.meilishuo.com")
+#print r.text
 
-class Fetcher:
-    def __init__(self, url):
-        self.response = b'' # Empty array of bytes.
-        self.url = url
-        self.sock = None
-
-        def fetch(self):
-            self.sock = socket.socket()
-            self.sock.setblocking(false)
-            try:
-                self.sock.connect(('xkcd.com',80))
-            except BlockingIOError:
-                pass
-
-            selector.register(self.sock.fileno(),
-                              EVENT_WRITE,
-                              self.connected)
-
-            def connected(self, key, mask):
-                print('connected!')
-                selector.unregister(key.fd)
-                request = 'GET {} HTTP/1.0\r\nHost: xkcd.com\r\n\r\n'.format(self.url)
-                self.sock.send(request.encode('ascii'))
-
-                selector.register(key.fd, EVENT_READ, self.read_response)
-
-    fetch('www.thinkhowblog.com');
+pattern = re.compile('[a-zA-z]+://[^\s]*')
+print pattern.findall(r.text)
